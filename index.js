492efeb6284b869a6ad4b7c13708a1be860e8955
fdebrabant <debrabant.fanny@gmail.com>
2021-02-03 19:29:24 +0100

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors());
 
 const movieRouter = require('./routes/movie.controller');
 
