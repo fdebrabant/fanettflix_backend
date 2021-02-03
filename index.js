e15844edb5c3830a756b59c9df47573252df23a3
fdebrabant <debrabant.fanny@gmail.com>
2021-02-03 12:25:39 +0100

    add img to public

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 app.use(express.json());
+app.use(express.static('public'));
 
 const movieRouter = require('./routes/movie.controller');
 
